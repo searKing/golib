ba3aada951b946f95b1617b72f8960a45b0406f8
陈海欣 <471030698@qq.com>
2018-09-22 22:57:33 +0800

    [ADD]add nil

--- util/object/nil.go
+++ util/object/nil.go
@@ -2,6 +2,7 @@ package object
 
 import (
 	"errors"
+	"reflect"
 	"strings"
 )
 
@@ -15,6 +16,37 @@ type Supplier interface {
 	Get() interface{}
 }
 
+// IsNil returns {@code true} if the provided reference is {@code nil} otherwise
+// returns {@code false}.
+func IsNil(obj interface{}) bool {
+	if IsNilAble(obj) {
+		return reflect.ValueOf(obj).IsNil()
+	}
+	return false
+}
+
+// IsNil returns {@code true} if the provided reference is non-{@code nil} otherwise
+// returns {@code false}.
+func NoneNil(obj interface{}) bool {
+	return !IsNil(obj)
+}
+
+// IsNil returns {@code true} if the provided reference can be assigned {@code nil} otherwise
+// returns {@code false}.
+func IsNilAble(obj interface{}) (canBeNil bool) {
+	reflect.ValueOf(obj).IsNil()
+
+	defer func() {
+		// As we can not access v.flag&reflect.flagMethod&v.ptr
+		// So we use recover() instead
+		if r := recover(); r != nil {
+			canBeNil = false
+		}
+	}()
+	canBeNil = true
+	return
+}
+
 // RequireNonNil checks that the specified object reference is not {@code nil}. This
 // method is designed primarily for doing parameter validation in methods
 // and constructors
@@ -22,28 +54,16 @@ func RequireNonNil(obj interface{}, msg ...string) interface{} {
 	if msg == nil {
 		msg = []string{"nil pointer"}
 	}
-	if obj == nil {
+	if IsNil(obj) {
 		panic(ErrorNilPointer(errors.New(strings.Join(msg, ""))))
 	}
 	return obj
 }
 
-// IsNil returns {@code true} if the provided reference is {@code nil} otherwise
-// returns {@code false}.
-func IsNil(obj interface{}) bool {
-	return obj == nil
-}
-
-// IsNil returns {@code true} if the provided reference is non-{@code nil} otherwise
-// returns {@code false}.
-func NoneNil(obj interface{}) bool {
-	return obj != nil
-}
-
 // RequireNonNullElse returns the first argument if it is non-{@code nil} and
 // otherwise returns the non-{@code nil} second argument.
 func RequireNonNullElse(obj, defaultObj interface{}) interface{} {
-	if obj != nil {
+	if NoneNil(obj) {
 		return obj
 	}
 	return RequireNonNil(defaultObj, "defaultObj")
@@ -52,8 +72,27 @@ func RequireNonNullElse(obj, defaultObj interface{}) interface{} {
 // RequireNonNullElseGet returns the first argument if it is non-{@code nil} and
 // returns the non-{@code nil} value of {@code supplier.Get()}.
 func RequireNonNullElseGet(obj interface{}, supplier Supplier) interface{} {
-	if obj != nil {
+	if NoneNil(obj) {
 		return obj
 	}
 	return RequireNonNil(RequireNonNil(supplier, "supplier").(Supplier).Get(), "supplier.Get()")
+}
+
+func IsEmptyValue(obj interface{}) bool {
+	v := reflect.ValueOf(obj)
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
 }
