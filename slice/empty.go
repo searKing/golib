901849e98b4ae7a5c09f250e25b8ae5ea1cd083a
陈海欣 <471030698@qq.com>
2018-09-24 03:32:03 +0800

    [ADD]add empty

--- slice/empty.go
+++ slice/empty.go
@@ -1,8 +1,8 @@
 package slice
 
 // EmptyFunc returns an empty sequential {@code slice}.
-func EmptyFunc() interface{} {
-	return emptyFunc()
+func EmptyFunc(s interface{}, ifStringAsRune ...bool) interface{} {
+	return normalizeSlice(emptyFunc(), s, ifStringAsRune...)
 }
 
 // emptyFunc is the same as EmptyFunc
