ad95ada96ea6e910141b6b4ce9fffdf23d8b1b9a
陈海欣 <471030698@qq.com>
2018-09-24 02:21:37 +0800

    [ADD]add min, max

---
+++ slice/max.go
@@ -0,0 +1,24 @@
+package slice
+
+import (
+	"github.com/searKing/golib/util/object"
+)
+
+// MaxFunc returns the maximum element of this stream according to the provided.
+func MaxFunc(s []interface{}, f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
+	return normalizeElem(minFunc(Of(s, ifStringAsRune...), f), s)
+
+}
+
+// maxFunc is the same as MaxFunc
+func maxFunc(s []interface{}, f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
+	object.RequireNonNil(s, "minFunc called on nil slice")
+	object.RequireNonNil(s, "minFunc called on nil callfn")
+
+	return ReduceFunc(s, func(left, right interface{}) interface{} {
+		if f(left, right) > 0 {
+			return left
+		}
+		return right
+	}, ifStringAsRune...)
+}
---
+++ slice/min.go
@@ -0,0 +1,24 @@
+package slice
+
+import (
+	"github.com/searKing/golib/util/object"
+)
+
+// MinFunc returns the minimum element of this stream according to the provided.
+func MinFunc(s []interface{}, f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
+	return normalizeElem(minFunc(Of(s, ifStringAsRune...), f), s)
+
+}
+
+// minFunc is the same as MinFunc
+func minFunc(s []interface{}, f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
+	object.RequireNonNil(s, "minFunc called on nil slice")
+	object.RequireNonNil(s, "minFunc called on nil callfn")
+
+	return ReduceFunc(s, func(left, right interface{}) interface{} {
+		if f(left, right) < 0 {
+			return left
+		}
+		return right
+	}, ifStringAsRune...)
+}
