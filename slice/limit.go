4fc170bf43e7e5b76d3631d40470db8ba5d5609d
陈海欣 <471030698@qq.com>
2018-09-23 18:43:53 +0800

    [ADD]add limit

---
+++ slice/limit.go
@@ -0,0 +1,21 @@
+package slice
+
+import "github.com/searKing/golib/util/object"
+
+// LimitFunc returns a slice consisting of the distinct elements (according to
+// {@link Object#equals(Object)}) of this slice.
+func LimitFunc(s []interface{}, maxSize int) []interface{} {
+	return limitFunc(s, maxSize)
+}
+
+// limitFunc is the same as DistinctFunc except that if
+//// truth==false, the sense of the predicate function is
+//// inverted.
+func limitFunc(s []interface{}, maxSize int) []interface{} {
+	object.RequireNonNil(s, "distinctFunc called on nil slice")
+	m := len(s)
+	if m > maxSize {
+		m = maxSize
+	}
+	return s[:m]
+}
