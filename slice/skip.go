a1ba0e620a7546ee3dfae0bcf86bc90e8645858f
陈海欣 <471030698@qq.com>
2018-09-24 01:02:32 +0800

    [ADD]add takewhile

--- slice/skip.go
+++ slice/skip.go
@@ -2,7 +2,7 @@ package slice
 
 import "github.com/searKing/golib/util/object"
 
-// Returns a slice consisting of the remaining elements of this slice
+// SkipFunc returns a slice consisting of the remaining elements of this slice
 // after discarding the first {@code n} elements of the slice.
 // If this slice contains fewer than {@code n} elements then an
 // empty slice will be returned.
---
+++ slice/takewhile.go
@@ -0,0 +1,26 @@
+package slice
+
+import "github.com/searKing/golib/util/object"
+
+// TakeWhileFunc returns, if this slice is ordered, a slice consisting of the longest
+// prefix of elements taken from this slice that match the given predicate.
+// Otherwise returns, if this slice is unordered, a slice consisting of a
+// subset of elements taken from this slice that match the given predicate.
+func TakeWhileFunc(s interface{}, f func(interface{}) bool, ifStringAsRune ...bool) interface{} {
+	return normalizeSlice(takeWhileFunc(Of(s, ifStringAsRune...), f, true))
+}
+
+// takeWhileFunc is the same as TakeWhileFunc.
+func takeWhileFunc(s []interface{}, f func(interface{}) bool, truth bool) []interface{} {
+	object.RequireNonNil(s, "takeWhileFunc called on nil slice")
+	object.RequireNonNil(f, "takeWhileFunc called on nil callfn")
+
+	var sTaken = []interface{}{}
+	for _, r := range s {
+		if f(r) != true {
+			break
+		}
+		sTaken = append(sTaken, r)
+	}
+	return sTaken
+}
