2e0bd1c8889b0de4fbf4bbfad573984c7adb6069
陈海欣 <471030698@qq.com>
2018-09-25 00:14:33 +0800

    [CHG]firendlly interface of stream

--- slice/stream.go
+++ slice/stream.go
@@ -1,114 +1,121 @@
 package slice
 
 type Stream struct {
-	s interface{}
+	s              interface{}
+	ifStringAsRune bool
 }
 
 func NewStream() *Stream {
 	return &Stream{}
 }
-func (stream *Stream) WithSlice(s interface{}) *Stream {
+func (stream *Stream) WithSlice(s interface{}, ifStringAsRune ...bool) *Stream {
 	stream.s = s
+	stream.ifStringAsRune = isAsRune(ifStringAsRune...)
 	return stream
 }
-func (stream *Stream) Filter(f func(interface{}) bool, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(FilterFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) SetIfStringAsRune(ifStringAsRune ...bool) *Stream {
+	stream.ifStringAsRune = isAsRune(ifStringAsRune...)
+	return stream
 }
 
-func (stream *Stream) Map(f func(interface{}) interface{}, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(MapFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) Filter(f func(interface{}) bool) *Stream {
+	return stream.WithSlice(FilterFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) Distinct(f func(interface{}, interface{}) int, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(DistinctFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) Map(f func(interface{}) interface{}) *Stream {
+	return stream.WithSlice(MapFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) Sorted(f func(interface{}, interface{}) int, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(SortedFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) Distinct(f func(interface{}, interface{}) int) *Stream {
+	return stream.WithSlice(DistinctFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) Peek(f func(interface{}), ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(PeekFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) Sorted(f func(interface{}, interface{}) int) *Stream {
+	return stream.WithSlice(SortedFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) Limit(maxSize int, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(LimitFunc(stream.s, maxSize, ifStringAsRune...))
+func (stream *Stream) Peek(f func(interface{})) *Stream {
+	return stream.WithSlice(PeekFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) Skip(n int, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(SkipFunc(stream.s, n, ifStringAsRune...))
+func (stream *Stream) Limit(maxSize int) *Stream {
+	return stream.WithSlice(LimitFunc(stream.s, maxSize, stream.ifStringAsRune))
 }
 
-func (stream *Stream) TakeWhile(f func(interface{}) bool, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(TakeWhileFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) Skip(n int) *Stream {
+	return stream.WithSlice(SkipFunc(stream.s, n, stream.ifStringAsRune))
 }
 
-func (stream *Stream) TakeUntil(f func(interface{}) bool, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(TakeUntilFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) TakeWhile(f func(interface{}) bool) *Stream {
+	return stream.WithSlice(TakeWhileFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) DropWhile(f func(interface{}) bool, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(DropWhileFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) TakeUntil(f func(interface{}) bool) *Stream {
+	return stream.WithSlice(TakeUntilFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) DropUntil(f func(interface{}) bool, ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(DropUntilFunc(stream.s, f, ifStringAsRune...))
+func (stream *Stream) DropWhile(f func(interface{}) bool) *Stream {
+	return stream.WithSlice(DropWhileFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) ForEach(f func(interface{}), ifStringAsRune ...bool) {
-	ForEachFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) DropUntil(f func(interface{}) bool) *Stream {
+	return stream.WithSlice(DropUntilFunc(stream.s, f, stream.ifStringAsRune))
 }
 
-func (stream *Stream) ForEachOrdered(f func(interface{}), ifStringAsRune ...bool) {
-	ForEachOrderedFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) ForEach(f func(interface{})) {
+	ForEachFunc(stream.s, f, stream.ifStringAsRune)
+}
+
+func (stream *Stream) ForEachOrdered(f func(interface{})) {
+	ForEachOrderedFunc(stream.s, f, stream.ifStringAsRune)
 }
 
 func (stream *Stream) ToSlice(ifStringAsRune ...bool) interface{} {
-	return ToSliceFunc(stream.s, ifStringAsRune...)
+	return ToSliceFunc(stream.s, stream.ifStringAsRune)
 }
 
-func (stream *Stream) Reduce(f func(left, right interface{}) interface{}, ifStringAsRune ...bool) interface{} {
-	return ReduceFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) Reduce(f func(left, right interface{}) interface{}) interface{} {
+	return ReduceFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) Min(f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
-	return MinFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) Min(f func(interface{}, interface{}) int) interface{} {
+	return MinFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) Max(f func(interface{}, interface{}) int, ifStringAsRune ...bool) interface{} {
-	return MaxFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) Max(f func(interface{}, interface{}) int) interface{} {
+	return MaxFunc(stream.s, f, stream.ifStringAsRune)
 }
 
 func (stream *Stream) Count(ifStringAsRune ...bool) int {
-	return CountFunc(stream.s, ifStringAsRune...)
+	return CountFunc(stream.s, stream.ifStringAsRune)
 }
 
-func (stream *Stream) AnyMatch(f func(interface{}) bool, ifStringAsRune ...bool) bool {
-	return AnyMatchFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) AnyMatch(f func(interface{}) bool) bool {
+	return AnyMatchFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) AllMatch(f func(interface{}) bool, ifStringAsRune ...bool) bool {
-	return AllMatchFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) AllMatch(f func(interface{}) bool) bool {
+	return AllMatchFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) NoneMatch(f func(interface{}) bool, ifStringAsRune ...bool) bool {
-	return NoneMatchFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) NoneMatch(f func(interface{}) bool) bool {
+	return NoneMatchFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) FindFirst(f func(interface{}) bool, ifStringAsRune ...bool) interface{} {
-	return FindFirstFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) FindFirst(f func(interface{}) bool) interface{} {
+	return FindFirstFunc(stream.s, f, stream.ifStringAsRune)
 }
 
-func (stream *Stream) FindAny(f func(interface{}) bool, ifStringAsRune ...bool) interface{} {
-	return FindAnyFunc(stream.s, f, ifStringAsRune...)
+func (stream *Stream) FindAny(f func(interface{}) bool) interface{} {
+	return FindAnyFunc(stream.s, f, stream.ifStringAsRune)
 }
 
 func (stream *Stream) Empty(ifStringAsRune ...bool) interface{} {
-	return EmptyFunc(stream.s, ifStringAsRune...)
+	return EmptyFunc(stream.s, stream.ifStringAsRune)
 }
 
 func (stream *Stream) Of(ifStringAsRune ...bool) *Stream {
-	return stream.WithSlice(Of(stream.s, ifStringAsRune...))
+	return stream.WithSlice(Of(stream.s, stream.ifStringAsRune))
 }
 
 func (stream *Stream) Concat(s2 *Stream) *Stream {
