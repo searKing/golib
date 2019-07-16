fc1e00585c2d72dcab747e5311d66cb546b87439
陈海欣 <471030698@qq.com>
2019-07-16 19:14:36 +0800

    [REFACTOR] taskstate

--- x/resillience/task.go
+++ x/resillience/task.go
@@ -40,19 +40,7 @@ func (t TaskType) String() string {
 	return b.String()
 }
 
-//go:generate stringer -type TaskState -trimprefix=TaskState
 
-type TaskState int
-
-const (
-	TaskStateNew               TaskState = iota // Task state for a task which has not yet started.
-	TaskStateRunning                            // Task state for a running task. A task in the running state is executing in the Go routine but it may be waiting for other resources from the operating system such as processor.
-	TaskStateDoneErrorHappened                  // Task state for a terminated state. The task has completed execution with some errors happened
-	TaskStateDoneNormally                       // Task state for a terminated state. The task has completed execution normally
-	TaskStateDormancy                           // Task state for a terminated state. The task has completed execution normally and will be started if New's called
-	TaskStateDeath                              // Task state for a terminated state. The task has completed execution normally and will never be started again
-	TaskStateButt
-)
 
 type Task struct {
 	Type        TaskType
